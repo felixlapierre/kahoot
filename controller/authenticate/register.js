5b3abd0d73f6bf1ef0fb5248deadaeb8d16ad57c
arejayelle <35614138+arejayelle@users.noreply.github.com>
2019-11-14 20:15:11 -0500

    Added Authentication code and database

--- controller/authenticate/register.js
+++ controller/authenticate/register.js
@@ -1,13 +1,11 @@
-var register = function(user,password) {
-    return true;
-    
-    if(user==="admin" && password==="password"){
+var register = function(username, password){
+        if (database.hasOwnProperty(username))
+        {
+            return false;
+        }
+        database[username] = password;
+        fs.writeFileSync("./authentication/users.json",JSON.stringify(database))
         return true;
-    }
-    else
-    {
-        return false;
-    }
 }
 
 module.exports=register;
